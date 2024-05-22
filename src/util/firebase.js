53ec7674e8dcc125b753e6fc949d51c0c0411edb
Josh Easley <easleyjs@gmail.com>
2024-05-21 21:36:14 -0700

    Fixes eslint semi

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,45 +1,45 @@
-import { QueryClient } from '@tanstack/react-query'
-import { initializeApp } from 'firebase/app'
-import { getFirestore, collection, doc, addDoc, getDocs, deleteDoc, updateDoc } from 'firebase/firestore'
+import { QueryClient } from '@tanstack/react-query';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, doc, addDoc, getDocs, deleteDoc, updateDoc } from 'firebase/firestore';
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient();
 
-const firebaseConfig = JSON.parse( import.meta.env.VITE_FIREBASE_CONFIG )
+const firebaseConfig = JSON.parse( import.meta.env.VITE_FIREBASE_CONFIG );
 
 export async function fetchEvents ( { signal, searchTerm } ) {
-  const app = initializeApp( firebaseConfig )
+  const app = initializeApp( firebaseConfig );
 
   // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore( app )
+  const db = getFirestore( app );
 
-  const data = await getDocs( collection( db, 'events' ) )
+  const data = await getDocs( collection( db, 'events' ) );
 
   const events = data.docs.map( ( doc ) => {
     return {
       ...doc.data(),
       id: doc.id
-    }
-  } )
+    };
+  } );
 
-  return events
+  return events;
 }
 
 export async function createNewEvent ( eventData ) {
   // Initialize Cloud Firestore and get a reference to the service
-  const app = initializeApp( firebaseConfig )
+  const app = initializeApp( firebaseConfig );
 
   // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore( app )
+  const db = getFirestore( app );
 
   try {
-    const docRef = await addDoc( collection( db, 'events' ), eventData )
+    const docRef = await addDoc( collection( db, 'events' ), eventData );
     // console.log( 'Document written with ID: ', docRef.id )
-    return docRef
+    return docRef;
   } catch ( e ) {
-    const error = new Error( 'An error occurred while creating the event' )
-    error.info = e
-    console.error( 'Error adding document: ', e )
-    throw error
+    const error = new Error( 'An error occurred while creating the event' );
+    error.info = e;
+    console.error( 'Error adding document: ', e );
+    throw error;
   }
 }
 
@@ -55,42 +55,42 @@ await updateDoc(washingtonRef, {
 */
 export async function updateEvent ( eventData ) {
   // Initialize Cloud Firestore and get a reference to the service
-  const app = initializeApp( firebaseConfig )
+  const app = initializeApp( firebaseConfig );
 
   // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore( app )
+  const db = getFirestore( app );
 
-  const docRef = doc( db, 'events', eventData.id )
-  const eventMinusId = { ...eventData }
-  delete eventMinusId.id
+  const docRef = doc( db, 'events', eventData.id );
+  const eventMinusId = { ...eventData };
+  delete eventMinusId.id;
 
   try {
-    const response = await updateDoc( docRef, eventMinusId )
-    console.log( 'Document updated: ', response )
+    const response = await updateDoc( docRef, eventMinusId );
+    console.log( 'Document updated: ', response );
 
-    return response
+    return response;
   } catch ( e ) {
-    const error = new Error( 'An error occurred while creating the event' )
-    error.info = e
-    console.error( 'Error adding document: ', e )
-    throw error
+    const error = new Error( 'An error occurred while creating the event' );
+    error.info = e;
+    console.error( 'Error adding document: ', e );
+    throw error;
   }
 }
 
 export async function deleteEvent ( { id } ) {
   // Initialize Cloud Firestore and get a reference to the service
-  const app = initializeApp( firebaseConfig )
+  const app = initializeApp( firebaseConfig );
 
   // Initialize Cloud Firestore and get a reference to the service
-  const db = getFirestore( app )
+  const db = getFirestore( app );
 
   try {
-    const response = await deleteDoc( doc( db, 'events', id ) )
-    return response
+    const response = await deleteDoc( doc( db, 'events', id ) );
+    return response;
   } catch ( e ) {
-    const error = new Error( 'An error occurred while deleting the event' )
-    error.info = e
-    console.error( 'Error deleting document: ', e )
-    throw error
+    const error = new Error( 'An error occurred while deleting the event' );
+    error.info = e;
+    console.error( 'Error deleting document: ', e );
+    throw error;
   }
 }
